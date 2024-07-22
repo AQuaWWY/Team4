fd1b657fdbc74ea4e91ba26150c3c29315dad124
AQuaWWY <131564131+AQuaWWY@users.noreply.github.com>
2024-07-22 17:31:28 +0800

    add Art

---
+++ FirstGame/Assets/Art/Pals/HitBoxPal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitBoxPal : MonoBehaviour
+{
+    GameObject parent;
+
+    private void Start()
+    {
+        parent = transform.parent.gameObject;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+		Vector2 hitDir = collision.transform.position - parent.transform.position;
+		hitDir = hitDir.normalized;
+		collision.gameObject.SendMessage("BeHit", new float[] { 1, hitDir.x, hitDir.y } );
+    }
+}
