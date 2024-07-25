f8d146d7bb895ed8ac7a197d72dc0e2ff299f15f
AQuaWWY <131564131+AQuaWWY@users.noreply.github.com>
2024-07-25 15:31:05 +0800

    血量UI

--- FirstGame/Assets/Scripts/EnemyController.cs
+++ FirstGame/Assets/Scripts/EnemyController.cs
@@ -8,6 +8,8 @@ public class EnemyController : MonoBehaviour
     public float moveSpeed;
     private Transform target;
     public float damage;
+    public float hitWaitTime = 1f;//受击间隔
+    private float hitCounter;//用于倒计时
 
     // Start is called before the first frame update
     void Start()
@@ -20,14 +22,20 @@ public class EnemyController : MonoBehaviour
     {
         theRB.velocity = (target.position - transform.position).normalized * moveSpeed;//设置敌人移动的向量
         //normalize使向量的大小化为之前设计好的长度1
+        if(hitCounter > 0)
+        {
+            hitCounter -= Time.deltaTime;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision) //触发when两个collider碰撞
     {
         //1.将player加入tag，函数发现敌人碰撞到对应tag的时候执行
-        if(collision.gameObject.tag == "Player")
+        if(collision.gameObject.tag == "Player" && hitCounter <= 0f)
         {
             PlayerHealthController.instance.TakeDamage(damage);
+
+            hitCounter = hitWaitTime;
         }
     }
 }
--- FirstGame/Assets/Scripts/PlayerHealthController.cs
+++ FirstGame/Assets/Scripts/PlayerHealthController.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;//调整UI的权限
 
 public class PlayerHealthController : MonoBehaviour
 {
-    public static PlayerHealthController instance;//所有敌人共用一个健康控制系统，不会在unity的inspetor中检查出来，因为不可以手动操作
+    public static PlayerHealthController instance;//所有敌人共用一个健康控制系统，不会在unity的inspector中检查出来，因为不可以手动操作
 
     private void Awake() //before start
     {
@@ -13,19 +14,25 @@ public class PlayerHealthController : MonoBehaviour
 
     public float currentHealth, maxHealth;
 
+    public Slider healthSlider;//Slider对象
+
     // Start is called before the first frame update
     void Start()
     {
+        //设置总血量和当前血量
         currentHealth = maxHealth;
+        
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = currentHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.T))//检测T键被按下就启动函数扣除伤害
-        {
-            TakeDamage(10f);
-        }
+        // if(Input.GetKeyDown(KeyCode.T))//检测T键被按下就启动函数扣除伤害
+        // {
+        //     TakeDamage(10f);
+        // }
     }
 
     public void TakeDamage(float damageToTake)//可以从其他脚本中调用
@@ -36,5 +43,7 @@ public class PlayerHealthController : MonoBehaviour
         {
             gameObject.SetActive(false);//deactivate the player
         }
+
+        healthSlider.value = currentHealth;//更新Slider当前血量
     }
 }
