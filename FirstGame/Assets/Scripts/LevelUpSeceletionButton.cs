7c3bfd5d5bf7aaecb72cd431d367e7db39a29946
AQuaWWY <131564131+AQuaWWY@users.noreply.github.com>
2024-07-31 21:17:08 +0800

    升级界面交互完成

--- FirstGame/Assets/Scripts/LevelUpSeceletionButton.cs
+++ FirstGame/Assets/Scripts/LevelUpSeceletionButton.cs
@@ -9,12 +9,27 @@ public class LevelUpSelectionButton : MonoBehaviour
     public TMP_Text upgradeDescText, nameLevelText;
     public Image weaponIcon;
 
+    private Weapon assignedWeapon;
+
     public void UpdateButtonDisplay(Weapon theWeapon)
     {
         upgradeDescText.text = theWeapon.stats[theWeapon.weaponLevel].upgradeText;
         weaponIcon.sprite = theWeapon.icon;
 
         nameLevelText.text = theWeapon.name + " - Lvl " + theWeapon.weaponLevel;
+
+        assignedWeapon = theWeapon;
+    }
+
+    public void SelectUpgrade()
+    {
+        if (assignedWeapon != null)//已分配的武器不为空
+        {
+            assignedWeapon.LevelUp();//执行升级
+
+            UIController.instance.levelUpPanel.SetActive(false);//关闭面板
+            Time.timeScale = 1f;//时间继续
+        }
     }
 
 }
